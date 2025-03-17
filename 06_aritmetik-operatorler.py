d1d98d24e7596d4d231690c08c1cd90bba764473
Symantec <167533660+Symantecw@users.noreply.github.com>
2025-03-17 16:34:05 +0300

    Create 06_aritmetik-operatorler.py

---
+++ 06_aritmetik-operatorler.py
@@ -0,0 +1,12 @@
+print(20 + 5)
+print(20 - 5)
+print(20 * 5)
+print(20 / 7)  # ondalıklı bölme yapar
+print(20 // 7)
+print(20 % 5 )  # mod alma
+print(20 ** 5)
+x = 20
+x+=3
+# x = x+ 3
+print(20 + 5 * 2)
+print(x)
