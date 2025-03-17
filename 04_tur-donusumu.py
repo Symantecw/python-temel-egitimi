3fc55bb426eee997e34be3ec8d7a10a4083d8c40
Symantec <167533660+Symantecw@users.noreply.github.com>
2025-03-17 16:31:04 +0300

    Create 04_tur-donusumu.py

---
+++ 04_tur-donusumu.py
@@ -0,0 +1,27 @@
+# Birinci örnek - Tam sayı:
+
+dogum_yili= int (input("Doğum yılınızı giriniz:"))
+yas =2024- dogum_yili
+print(yas) 
+
+
+
+# İkinci örnek - Tam sayı:
+
+"""
+ birinci_sayi =int (input("Birinci sayıyı giriniz;"))
+ikinci_sayi = int (input("İkinci sayıyı giriniz:"))
+toplam = birinci_sayi + ikinci_sayi
+print("Toplam:",toplam) 
+"""
+
+
+
+# Üçüncü örnek ondalıklı sayı:
+
+birinci_sayi =float (input("Birinci sayıyı giriniz;"))
+ikinci_sayi = float (input("İkinci sayıyı giriniz:"))
+toplam = birinci_sayi + ikinci_sayi
+print("Toplam:",toplam) 
+
+
