6de5134765078854ddb15786f0f5d10096f31679
Symantec <167533660+Symantecw@users.noreply.github.com>
2025-03-17 16:32:48 +0300

    Create 05_stringler.py

---
+++ 05_stringler.py
@@ -0,0 +1,7 @@
+kurs_ismi = "python programlama dili"
+print(kurs_ismi)
+print(kurs_ismi.upper())
+print(kurs_ismi.lower())
+print(kurs_ismi.find("p"))
+print(kurs_ismi.replace("python","java"))
+print("programlama" in kurs_ismi)
