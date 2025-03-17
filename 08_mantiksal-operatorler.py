68675fa98f3364f6c8c6e1e79060263a5994dff8
Symantec <167533660+Symantecw@users.noreply.github.com>
2025-03-17 16:36:32 +0300

    Create 08_mantiksal-operatorler.py

---
+++ 08_mantiksal-operatorler.py
@@ -0,0 +1,7 @@
+fiyat = 10
+print(fiyat > 5 and fiyat < 15 )
+# and operatörü iki koşulunda doğru olması durumunda true döndürür
+print(fiyat > 11 or fiyat < 15)
+# or operatörü iki koşuldan birinin doğru olması surumunda true döndürür
+print(not(fiyat > 5 ))
+# not operatörü koşulu tersine çevirir
